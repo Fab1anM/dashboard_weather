49b1eaac4cd065391c9c92f0494231391f04cfc3
Fabian Mirz <fabian.mirz@pexon-consulting.de>
2026-07-01 00:35:47 +0200

    fix for raspberry

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 set -e
 
-# Xvfb starten (virtueller Display :1, 1920x1080)
-Xvfb :1 -screen 0 1920x1080x24 &
-XVFB_PID=$!
-sleep 2
-
 # FastAPI Backend starten auf Port 8000
 dashboard-weather &
 APP_PID=$!
@@ -19,20 +14,28 @@ for i in $(seq 1 10); do
     sleep 1
 done
 
-# Chromium im Kiosk-Modus
-DISPLAY=:1 chromium \
+# Chromium im Kiosk-Modus (direkt über DRM/KMS)
+chromium \
     --kiosk \
     --no-sandbox \
-    --disable-gpu \
+    --use-gl=egl \
+    --disable-gpu-compositing \
     --disable-infobars \
     --disable-features=TranslateUI \
     --disable-session-crashed-bubble \
     --disable-pinch \
     --disable-restore-session-state \
-    http://localhost:8000 &
+    --no-first-run \
+    --disable-background-timer-throttling \
+    --disable-backgrounding-occluded-windows \
+    --disable-renderer-backgrounding \
+    --disable-extensions \
+    --disable-default-apps \
+    --disable-sync \
+    --disable-translate \
+    --kiosk http://localhost:8000 &
 BROWSER_PID=$!
 
-# Auf Signale reagieren
+# Auf alle Prozesse warten
 wait $APP_PID
-wait $XVFB_PID
 wait $BROWSER_PID
